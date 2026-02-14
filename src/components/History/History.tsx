2170347fa53ef54381a97bd26baaad0bd36c3899
Willian Oliveira <willian.oliveira737@gmail.com>
2026-02-14 20:00:11 -0300

    feat: adjust styles, i18n

--- src/components/History/History.tsx
+++ src/components/History/History.tsx
@@ -3,10 +3,26 @@ import "./History.css";
 
 export const History = () => {
   const { t } = useTranslation();
+  const empty = false;
 
   return (
     <section id="history">
-      <div>{t("history.empty")}</div>
+      {empty ? (
+        <div>{t("history.empty")}</div>
+      ) : (
+        <>
+          <div className="chip-history">PR-GUM</div>
+          <div className="chip-history">PR-EBJ</div>
+          <div className="chip-history">PS-NJR</div>
+          <div className="chip-history">PR-JRY</div>
+          <div className="chip-history">PR-XBV</div>
+          <div className="chip-history">7T-VPS</div>
+          <div className="chip-history">N607UP</div>
+          <div className="chip-history">VH-XZJ</div>
+          <div className="chip-history">N844FD</div>
+          <div className="chip-history">D-ABYS</div>
+        </>
+      )}
     </section>
   );
 };
--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -3,10 +3,35 @@ import "./Result.css";
 
 export const Result = () => {
   const { t } = useTranslation();
+  const imageUrl = "https://cdn.jetphotos.com/full/5/907826_1765980816.jpg";
 
   return (
     <section id="result">
-      <div></div>
+      <div className="image-result">
+        <img src={imageUrl} alt="result image" />
+      </div>
+      <div className="data-result">
+        <h2>PR-XBV</h2>
+        <div className="data-row">
+          <span className="label">{t("result.manufacturer")}</span>
+          <span className="value">Airbus</span>
+        </div>
+
+        <div className="data-row">
+          <span className="label">{t("result.operator")}</span>
+          <span className="value">LATAM Airlines</span>
+        </div>
+
+        <div className="data-row">
+          <span className="label">{t("result.model")}</span>
+          <span className="value">A320-271N</span>
+        </div>
+
+        <div className="data-row">
+          <span className="label">{t("result.year")}</span>
+          <span className="value">2006</span>
+        </div>
+      </div>
     </section>
   );
 };
