859f6c42c95fdcd6213debde4bc17ce2c5136db8
Willian Oliveira <willian.oliveira737@gmail.com>
2026-09-05 12:54:43 -0300

    feat: Add header with input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
 import "./App.css";
 
+const searchAircraft = () => {
+  console.log("Searching for aircraft...");
+};
+
 function App() {
   return (
     <>
-      <h1>WingsData</h1>
+      <header>
+        <h1>WingsData</h1>
+      </header>
+      <section>
+        <main>
+          <p className="message">
+            Digite o registro da aeronave
+            {/* <br />
+            Type the aircraft registration */}
+          </p>
+          <div>
+            <input type="text" id="registration" name="registration" />
+            <button
+              type="submit"
+              className="search-btn"
+              aria-label={"Search"}
+              disabled={false}
+              onClick={searchAircraft}
+            >
+              <img src="./btn-airplane.png" alt={"Search"} />
+            </button>
+          </div>
+        </main>
+      </section>
     </>
   );
 }
